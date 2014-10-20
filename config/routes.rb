aecc405ae52de4a2cdcd8e467af4cfa297851fd0
alfonsopintos <alfonsopintos@gmail.com>
2014-10-20 16:16:58 -0400

    clients root page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :clients
 
   devise_for :users
+
+  root 'clients#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
