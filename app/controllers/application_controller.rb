750b90e36633f3353d2034869e2b81f5a3cf23cd
alfonsopintos <alfonsopintos@gmail.com>
2014-10-22 18:23:40 -0400

    added first and last name devise fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected 
+  def configure_permitted_parameters
+    [:sign_up, :account_update].each do |action|
+      devise_parameter_sanitizer.for(action).push(:first_name, :last_name)
+    end
+  end
+
 end
