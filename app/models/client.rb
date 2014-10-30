7dc0839699229884862098845f0c7b8d25be41be
alfonsopintos <alfonsopintos@gmail.com>
2014-10-29 22:18:14 -0400

    new large format in paperclip. grid system in all view pages.

--- app/models/client.rb
+++ app/models/client.rb
@@ -7,7 +7,7 @@ class Client < ActiveRecord::Base
   validates :open_time, presence: true
   validates :close_time, presence: true
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "375x375>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 end
